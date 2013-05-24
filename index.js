0081e294be49e6046104e9c3ed745206560cc645
Blaine Bublitz <blaine@iceddev.com>
2013-05-24 15:27:57 -0700

    use socket.io room manager instead of special array

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var io = require('socket.io').listen(11097);
 
 io.sockets.on('connection', function (socket) {
-  socket.joinedChannels = [];
 
   socket.on('getChan',function(obj){
     console.log('hello getChan request', obj);
@@ -12,15 +11,12 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('broadcast', function(obj){
     socket.broadcast.to(obj.id).emit('broadcast', obj); //dont send back to self
-    if(this.request){
-      this.resultCB('ok');
-    }
   });
 
   socket.on('joinChannel', function(id){
-    if(socket.joinedChannels.length < 5){
+    var roomsJoined = Object.keys(io.sockets.manager.roomClients[socket.id]);
+    if(roomsJoined.length < 5){
       socket.join(id);
-      socket.joinedChannels.push(id);
       socket.emit('joined', id);
     }
   });
